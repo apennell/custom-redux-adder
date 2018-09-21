0b3ab404196fab62285bdd6e54b8e8b153ad2875
Annie Pennell <anniecpennell@gmail.com>
2018-09-21 14:16:04 -0700

    Added amount to increment and decrement actions

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 function reducer(state, action) {
-  if (action.type === 'INCREMENT') { return state + 1 }
-  if (action.type === 'DECREMENT') { return state - 1 }
+  if (action.type === 'INCREMENT') { return state + action.amount }
+  if (action.type === 'DECREMENT') { return state - action.amount }
 
   return state;
 }
 
-const incrementAction = { type: 'INCREMENT' };
+const incrementAction = {
+  type: 'INCREMENT',
+  amount: 7,
+};
 
 console.log(reducer(0, incrementAction));
 console.log(reducer(1, incrementAction));
 console.log(reducer(5, incrementAction));
 
-const decrementAction = { type: 'DECREMENT' };
+const decrementAction = {
+  type: 'DECREMENT',
+  amount: 7,
+};
 
 console.log(reducer(10, decrementAction));
 console.log(reducer(9, decrementAction));
 console.log(reducer(5, decrementAction));
 
-const unknownAction = { type: 'UKNOWN' };
+const unknownAction = { type: 'UNKNOWN' };
 
 console.log(reducer(5, unknownAction));
 console.log(reducer(8, unknownAction));
